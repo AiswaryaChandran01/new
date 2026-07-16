0147921a47acfb32826ecdb3b8e7004764f293f3
Aiswarya Chandran <aiswaryachamdran000@gmail.com>
2026-07-16 15:09:06 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,85 @@ function App() {
   const [password, setPassword] = useState("");
 
   function login() {
-
     if (username === "admin" && password === "1234") {
       alert("Login Successful");
     } else {
       alert("Invalid Username or Password");
     }
-
   }
 
   return (
-    <div>
 
-      <h1>Login Form</h1>
+    <div
+      style={{
+        width: "350px",
+        margin: "100px auto",
+        padding: "20px",
+        textAlign: "center",
+        backgroundColor: "#6484be",
+        borderRadius: "10px",
+        boxShadow: "0px 0px 10px gray"
+      }}
+    >
+
+      <h1
+        style={{
+          color: "black"
+        }}
+      >
+        Login Form
+      </h1>
 
       <input
         type="text"
         placeholder="Enter Username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        style={{
+          width: "90%",
+          padding: "10px",
+          marginBottom: "15px",
+          borderRadius: "5px",
+          border: "1px solid gray"
+        }}
       />
 
-      <br /><br />
+      <br />
 
       <input
         type="password"
         placeholder="Enter Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        style={{
+          width: "90%",
+          padding: "10px",
+          marginBottom: "15px",
+          borderRadius: "5px",
+          border: "1px solid gray"
+        }}
       />
 
-      <br /><br />
+      <br />
 
-      <button onClick={login}>
+      <button
+        onClick={login}
+        style={{
+          backgroundColor: "blue",
+          color: "white",
+          padding: "10px 20px",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+          fontSize: "16px"
+        }}
+      >
         Login
       </button>
 
     </div>
-  );
 
+  );
 }
 
 export default App;
